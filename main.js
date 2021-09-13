76250f88a0d5342d8ab582628cddb8ab904f78d6
codu2 <82283517+codu2@users.noreply.github.com>
2021-09-14 06:07:15 +0900

    update, edit main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const SEARCH_URL = BASE_URL + `/search/movie?` + API_KEY;
 const DRAMA_URL = `/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&`;
 const DRAMA_JONE = BASE_URL + DRAMA_URL + API_KEY;
 const HIGH_URL = `/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&`;
-const HIGH_MOVIE = BASE_URL + HIGH_URL + API_KEY;
+const HIGH_DRAMA = BASE_URL + HIGH_URL + API_KEY;
 
 
 const main = document.getElementById('main');
@@ -102,10 +102,10 @@ function showDrama(dramas) {
     drama.innerHTML = html;
 }
 
-const highestMovie = document.querySelector('.highest-rated-movies');
-const highestMovies = document.querySelectorAll('.highest');
+const highestDrama = document.querySelector('.highest-rated-drama');
+const highestDramas = document.querySelectorAll('.highest');
 
-getHighest(HIGH_MOVIE);
+getHighest(HIGH_DRAMA);
 
 function getHighest(url) {
 
@@ -116,25 +116,25 @@ function getHighest(url) {
     })
 }
 
-function showHighest(highestMovies) {
+function showHighest(highestDramas) {
     let html = '';
 
-    highestMovies.forEach(hiMovie => {
-        if (hiMovie) {
-            console.log(hiMovie.poster_path);
-            if (hiMovie.poster_path !== null) {
+    highestDramas.forEach(hiDrama => {
+        if (hiDrama) {
+            console.log(hiDrama.poster_path);
+            if (hiDrama.poster_path !== null) {
                 html += `<div class="highest">
-                        <img src="${IMG_URL + hiMovie.poster_path}" alt="image">
+                        <img src="${IMG_URL + hiDrama.poster_path}" alt="image">
                         <div class="highest-info">
-                            <h3>${hiMovie.title}</h3>
-                            <span class="vote">${hiMovie.vote_average}</span>
+                            <h3>${hiDrama.title}</h3>
+                            <span class="vote">${hiDrama.vote_average}</span>
                         </div>
                     </div>`
             }
         }
     })
-
-    highestMovie.innerHTML = html;
+    
+    highestDrama.innerHTML = html;
 }
 
 const signUpBtn = document.querySelector('.sign-up-button');
@@ -145,6 +145,7 @@ const showSign = document.querySelector('.sign-in');
 const signContainer = document.querySelector('.sign-container');
 const menu = document.querySelector('.menu i');
 const nav = document.querySelector('.nav');
+const signCloseBtn = document.querySelector('.sign-close')
 
 signUpBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -159,6 +160,11 @@ signInBtn.addEventListener('click', (e) => {
 showSign.addEventListener('click', (e) => {
     e.preventDefault();
    signContainer.classList.toggle('active');
+})
+
+signCloseBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+   signContainer.classList.remove('active');
 })
 
 menu.addEventListener('click', () => {
@@ -181,7 +187,7 @@ let pressed = false; /*클릭되었는지 아닌지를 체크하는 변수 - def
 let startX; /*마우스 드래그 시작점 x좌표 체크 - 쉽게 말하면 이미지를 grab하여 이미지를 움직이려고 할 때 grab한 지점의 x좌표를 체크하는 변수*/
 let x; /*마우스 드래그 시 x좌표 체크 - 즉 이미지를 grab하여 왼쪽으로 움직이면, 움직여 이동한 그 지점의 x좌표를 체크하는 변수*/
 
-/*즉 startX는 처음에 드래그하려고 이미지를 잡은 지점.
+/*즉 startX는 처음에 드래그하려고 이미지를 잡은 지점. 문서 맨 왼쪽이 아닌 요소의 맨 왼쪽 기준
 x는 innerslider.style.left = '0px'을 기준으로 내 마우스가 최종적으로 위치한 지점의 좌표*/
 
 slider.addEventListener('mousedown', e => {
@@ -236,3 +242,47 @@ slider.addEventListener('mousemove', e => {
 })
 /*drag를 처리하는 이벤트로, preventDefault() 는 현재 이벤트의 기본 동작을 중단시키는 함수 
 이 이벤트가 실행되는 즉시 위에서 만든 checkboundary() 함수를 호출하여 영역 내부 처리가 맞는지 확인해줌*/
+
+let slider2 = document.querySelector('.highest-drama-zone');
+let innerslider2 = document.querySelector('.highest-rated-drama');
+let pressed2 = false;
+let startX2; 
+let x2; 
+
+slider2.addEventListener('mousedown', e => {
+    pressed2 = true; 
+    startX2 = e.offsetX - innerslider2.offsetLeft; 
+    slider2.style.cursor = "grabbing"; 
+})
+
+slider2.addEventListener('mouseenter', () => {
+    slider2.style.cursor = "grab" 
+})
+
+slider2.addEventListener('mouseup', () => {
+    slider2.style.cursor = "grab"
+})
+
+window.addEventListener('mouseup', () => {
+    pressed2 = false;
+})
+
+function checkBoundary2() {
+    let outer = slider2.getBoundingClientRect();
+    let inner = innerslider2.getBoundingClientRect();
+
+    if(parseInt(innerslider2.style.left) > 0) {
+        innerslider2.style.left = '0px';
+    } else if (inner.right < outer.right) {
+        innerslider2.style.left = `-${inner.width - outer.width}px`
+    }
+}
+
+slider2.addEventListener('mousemove', e => {
+    if(!pressed2) return 
+    e.preventDefault();
+    x2 = e.offsetX;
+
+    innerslider2.style.left = `${x2 - startX2}px`;
+    checkBoundary2();
+})
